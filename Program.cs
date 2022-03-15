be9488a8096aa7a0bf2745168ba3d92f5db191ee
chandana03 <chandanareshma01@gmail.com>
2022-03-15 22:25:16 +0530

    push values

--- Program.cs
+++ Program.cs
@@ -6,47 +6,42 @@ namespace stackdemo
     class Program
     {
         static void Main(string[] args)
-        {
-            Stack<int> st = new Stack<int>();
-
-            st.Push(70);
-            st.Push(30);
-            st.Push(56);
-
-
-            foreach (int ele in st)
-            {
-                Console.WriteLine(ele);
-            }
-            Console.WriteLine("----------");
-            Console.WriteLine("peek value " + st.Peek());
-            Console.WriteLine("----------");
-            st.Pop();
-            foreach (int ele in st)
-            {
-                Console.WriteLine(ele);
-            }
-            Console.WriteLine("--------");
-
-            Console.WriteLine("peek value " + st.Peek());
-            Console.WriteLine("----------");
-            st.Pop();
-            foreach (int ele in st)
-            {
-                Console.WriteLine(ele);
-            }
-            Console.WriteLine("--------");
-
-            Console.WriteLine("peek value " + st.Peek());
-            Console.WriteLine("----------");
-            st.Pop();
-            foreach (int ele in st)
-            {
-                Console.WriteLine(ele);
-            }
-           
-           
+        { 
             
+
+           
+              Queue<int> queue = new Queue<int>();
+
+                queue.Enqueue(56);
+                queue.Enqueue(30);
+                queue.Enqueue(70);
+                
+
+                foreach (int element in queue)
+                {
+                    Console.WriteLine(element);
+                }
+           
+            Console.WriteLine("peek value is " + queue.Peek());
+
+            //Console.WriteLine("--------------");
+            //queue.Dequeue();
+            //foreach (int element in queue)
+            //{
+            //    Console.WriteLine(element);
+            //}
+            //Console.WriteLine("peek value is " + queue.Peek());
+            //Console.WriteLine("--------------");
+
+            //queue.Dequeue();
+            //foreach (int element in queue)
+            //{
+            //    Console.WriteLine(element);
+            //}
+            //Console.WriteLine("peek value is " + queue.Peek());
+
+
+
 
 
 
